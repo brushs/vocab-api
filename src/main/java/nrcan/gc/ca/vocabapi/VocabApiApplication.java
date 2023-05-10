86960c8d47b3bb0644f59a0dc756337a549c41b9
steve.brush@apption.com <steve.brush@apption.com>
2023-05-10 15:17:29 -0400

    Initial commit

---
+++ src/main/java/nrcan/gc/ca/vocabapi/VocabApiApplication.java
@@ -0,0 +1,13 @@
+package nrcan.gc.ca.vocabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nrcan/gc/ca/vocabapi/VocabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nrcan.gc.ca.vocabapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
